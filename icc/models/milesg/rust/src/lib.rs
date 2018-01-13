e7c32f3f5fb36d9fdcc4c90271f8c020c5875159
Miles Granger <miles59923@gmail.com>
2018-01-13 16:29:59 +0100

    added rust-lang model with Python interface

---
+++ icc/models/milesg/rust/src/lib.rs
@@ -0,0 +1,79 @@
+#![feature(proc_macro, specialization)]
+
+extern crate rusty_machine as rm;
+extern crate pyo3;
+
+use pyo3::prelude::*;
+
+use rm::learning::logistic_reg::LogisticRegressor;
+use rm::learning::SupModel;
+use rm::linalg::{Matrix, Vector};
+use std::vec::Vec;
+use std::option::Option;
+
+
+
+#[py::modinit(models)]
+// Initialize the 'alterations' module of lumberjack
+fn init_mod(py: Python, m: &PyModule) -> PyResult<()> {
+
+    #[pyfn(m, "logistic_regression")]
+    fn logistic_regression_py(mut xTest: Vec<Vec<f64>>,
+                              mut xTrain: Vec<Vec<f64>>,
+                              mut yTrain: Vec<f64>) -> PyResult<Option<Vec<f64>>> {
+        /*
+            Python interface to the logistic regression model
+        */
+        let result = train_n_predict_log(xTest, xTrain, yTrain);
+        Ok(result)
+    }
+
+    Ok(())
+}
+
+
+fn train_n_predict_log(mut xTest: Vec<Vec<f64>>,
+                       mut xTrain: Vec<Vec<f64>>,
+                       mut yTrain: Vec<f64>) -> Option<Vec<f64>> {
+    /*
+        Implement training and predicting of a vanilla Logistic Regression model
+    */
+    let xTest: Matrix<f64> = convert_x_to_matrix(xTest);
+    let xTrain: Matrix<f64> = convert_x_to_matrix(xTrain);
+    let yTrain: Vector<f64> = Vector::new(yTrain);
+
+    let mut model = LogisticRegressor::default();
+
+    // Train and predict if training was successful
+    if let Err(err) = model.train(&xTrain, &yTrain) {
+        println!("Error training model: {}", err);
+        None
+
+    } else {
+
+        // Training was successful, now predict on test
+        if let Ok(out) = model.predict(&xTest) {
+
+            // Return rm::linalg::Vector as std::vec::Vector
+            Some(out.iter().map(|x: &f64| *x).collect::<Vec<f64>>())
+
+        // Prediction failed...
+        } else {
+            None
+        }
+    }
+}
+
+
+fn convert_x_to_matrix(x: Vec<Vec<f64>>) -> Matrix<f64> {
+    /*
+        Convert the passed nested vectors to the rusty_machine Matrix type
+    */
+    let (n_rows, n_cols) = (x.len(), x[0].len());
+    let mut inputs = Vec::with_capacity(n_rows * n_cols);
+    for row in x {
+        inputs.extend(row);
+    }
+    Matrix::new(n_rows, n_cols, inputs)
+}
+
---
+++ icc/models/rusty_logistic.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import pandas as pd
+
+from sklearn.base import BaseEstimator
+from sklearn.preprocessing import QuantileTransformer, Imputer
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+
+try:
+    # Try to import the compiled rust extension, if not, try to build it.
+    from icc.models.milesg.rust.rust_models.rust import models
+except ImportError:
+    from subprocess import Popen, PIPE
+    import warnings
+    warnings.warn('Unable to import compiled rust extension, trying to build it!')
+    dir = os.path.dirname(os.path.abspath(__file__))
+    rust_setup_dir = os.path.join(dir, 'milesg', 'rust')
+    with Popen(['bash', '-c', 'cd {} && python setup.py develop'.format(rust_setup_dir)], stdout=PIPE) as p:
+        for line in p.stdout:
+            print(line, end='')
+    if p.returncode == 0:
+        from icc.models.milesg.rust.rust_models.rust import models
+    else:
+        raise OSError('Unable to build rust extension!')
+
+
+class RustyLogistic(BaseEstimator):
+    """
+    Concept model, to demonstrate ability to connect to rust-lang model.
+    """
+
+    def __init__(self):
+        """
+        Interface to Rust logistic regression
+        """
+
+    def get_params(self, deep=True):
+        return {}
+
+    def _format_x(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Format x so that it represents unnested arrays of source data
+
+        Parameters
+        ----------
+        X: pd.DataFrame - Data frame resulting form the icc.data_loader.DataLoader class
+
+        Returns
+        -------
+        pd.DataFrame with each pixel from each band in it's own column
+        """
+        bands = ['band_1', 'band_2']
+        for band in bands:
+            unstacked = X[band].apply(pd.Series).stack().unstack()
+            unstacked.columns = ['{}_{}'.format(band, i) for i in range(unstacked.shape[1])]
+            X = X.join(unstacked)
+
+        X.drop(columns=bands + ['inc_angle'], inplace=True)
+        return X
+
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+        """
+        Fit to training data
+        """
+        self.xTrain = self._format_x(X.copy())
+        self.yTrain = y.copy()
+
+
+        # Pipeline
+        self.pipeline = Pipeline(steps=[
+            ('qt', QuantileTransformer(output_distribution='normal')),
+            ('pca', PCA(200))
+        ])
+        self.xTrain = self.pipeline.fit_transform(self.xTrain)
+
+        return self
+
+    def predict(self, X: pd.DataFrame) -> np.ndarray:
+        """
+        Get classification predictions for X
+        """
+        probs = self.predict_proba(X)
+        return np.array((1 if p[1] > 0.5 else 0 for p in probs))
+
+    def predict_proba(self, X: pd.DataFrame) -> np.ndarray:
+        """
+        Predict probability for X
+
+        'rusty-machine' presently has no way to save a trained model, so we're sending over training with
+        the testing data at once.
+        """
+        X = self._format_x(X.copy())
+        X = self.pipeline.transform(X)
+        output = models.logistic_regression(X, self.xTrain, self.yTrain.values)
+        return np.array(output)
